bd43a3de038bddc4693fac1a8f00ae2f8f86fc01
awong2025 <59582940+awong2025@users.noreply.github.com>
2020-01-09 18:28:21 -0500

    Update AutonomousBlueAlternatePark.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueAlternatePark.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueAlternatePark.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "autonomousBlue (Blocks to Java)", group = "")
+@Autonomous(name = "AutonomousBlueAlternatePark", group = "")
 public class AutonomousBlueAlternatePark extends LinearOpMode {
 
     private Servo HornServoR;
