6dae492253b0b17f0e43ad6eff3b48590b02baef
awong2025 <59582940+awong2025@users.noreply.github.com>
2020-01-09 18:30:53 -0500

    Update AutonomousRed.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousRed.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "autonomousRed (Blocks to Java)", group = "")
+@Autonomous(name = "AutonomousRed", group = "")
 public class AutonomousRed extends LinearOpMode {
 
     private DcMotor leftRear;
