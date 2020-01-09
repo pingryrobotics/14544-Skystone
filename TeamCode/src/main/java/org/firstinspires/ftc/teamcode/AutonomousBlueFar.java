ec3eb59602ef567e5163228c0b8c7ae716728ae8
awong2025 <59582940+awong2025@users.noreply.github.com>
2020-01-09 18:51:22 -0500

    Update AutonomousBlueFar.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueFar.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBlueFar.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 
-@Autonomous(name = "autonomousBlueFar (Blocks to Java)", group = "")
+@Autonomous(name = "AutonomousBlueFar", group = "")
 public class AutonomousBlueFar extends LinearOpMode {
 
     private Servo HornServoR;
